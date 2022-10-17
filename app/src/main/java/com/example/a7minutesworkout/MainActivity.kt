769b1ca45077a934da7fb8b8c413322b05920750
IsikParlar <m.parlar@hotmail.com>
2022-10-18 01:45:39 +0300

    UI Finished

---
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener {
+
+        }
+    }
+}
