c374729a6dd778907b40e12ab2a49fd3d5bac48a
IsikParlar <m.parlar@hotmail.com>
2022-10-31 05:07:10 +0300

    Beta v.1

--- app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
@@ -2,7 +2,11 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     private var binding : ActivityFinishBinding?=null
@@ -23,6 +27,20 @@ class FinishActivity : AppCompatActivity() {
 
         binding?.btnFinish?.setOnClickListener {
             finish()
+        }
+    }
+
+    val dao = (application as WorkOutApp).db.historyDao()
+
+    private fun addDatabaseToDatabase(historyDAO: HistoryDAO){
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss",Locale.getDefault())
+        val date = sdf.format(dateTime)
+
+        lifecycleScope.launch {
+            historyDAO.insert(HistoryEntity(date))
         }
     }
 }
--- app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -2,10 +2,14 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
-    private var binding : ActivityHistoryBinding? = null
+    private var binding: ActivityHistoryBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +17,7 @@ class HistoryActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         setSupportActionBar(binding?.toolbarHistoryActivity)
-        if (supportActionBar!=null){
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
             supportActionBar?.title = "HISTORY"
         }
@@ -21,5 +25,40 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        val dao = (application as WorkOutApp).db.historyDao()
+        getAllCompletedDates(dao)
+    }
+
+    private fun getAllCompletedDates(historyDAO: HistoryDAO) {
+        lifecycleScope.launch {
+            historyDAO.fetchAllDates().collect { allCompletedDatesList ->
+                if(allCompletedDatesList.isNotEmpty()) {
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+                    val dates = ArrayList<String>()
+
+                    for (date in allCompletedDatesList){
+                        dates.add(date.date)
+                    }
+                    val historyAdapter = HistoryAdapter(dates)
+                    binding?.rvHistory?.adapter = historyAdapter
+                }
+                else{
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+                }
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
